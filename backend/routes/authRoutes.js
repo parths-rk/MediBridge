d0bdfd0d6985862bada08fe550a84abafa795f93
Parth <parths.singh2003@gmail.com>
2025-09-07 03:11:23 +0530

    Added Authorization Routes

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,55 @@
+import express from "express";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+import { protect } from "../middleware/auth.js";
+
+const router = express.Router();
+
+//Get logged in info
+router.get("/me",protect,async (req,res)=>{
+  res.json(req.user);
+});
+
+// Register
+router.post("/register", async (req, res) => {
+  try {
+    const { name, email, password, role } = req.body;
+    if (!name || !email || !password) return res.status(400).json({ message: "Missing fields" });
+
+    const exists = await User.findOne({ email });
+    if (exists) return res.status(409).json({ message: "Email already registered" });
+
+    const hash = await bcrypt.hash(password, 10);
+
+    const user = await User.create({ name, email, password: hash, role: role || "patient" });
+    res.status(201).json({
+      id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+      specialization: user.specialization,
+    });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// Login
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) return res.status(401).json({ message: "Invalid credentials" });
+
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) return res.status(401).json({ message: "Invalid credentials" });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "7d" });
+    res.json({ token });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+export default router;
